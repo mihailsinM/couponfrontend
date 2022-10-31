215627c15bba43ae75c530e33c91eedc25340e27
misha <mihailsin49@gmail.com>
2022-10-31 14:12:52 +0200

    v1

--- src/Components/States/AuthState.ts
+++ src/Components/States/AuthState.ts
@@ -17,13 +17,16 @@ export interface AuthActions {
 export class CompanyState {
 
     public token: string;
-    public company: CompanyModel;
-    public admin: AdminModel;
-    public customer: CustomerModel;
+    // public company: CompanyModel;
+    // public admin: AdminModel;
+    // public customer: CustomerModel;
     public creds: Credentials;
 
     constructor() {
-        this.token = localStorage.getItem("token");
+        if(localStorage.getItem("token")){
+            this.token = localStorage.getItem("token");
+            
+        }
     }
 }
 
@@ -47,12 +50,12 @@ export function reducer(currentState: CompanyState, action: AuthActions) {
         case AuthActionsTypes.Register:
             newState.token = action.payload;
             localStorage.setItem("token", newState.token)
-            const tokenObject: CompanyModel = jwtDecode(newState.token);
-            newState.company = tokenObject;
+            const tokenObject: Credentials = jwtDecode(newState.token);
+            newState.creds = tokenObject;
             break;
         case AuthActionsTypes.Logout:
             newState.token = null;
-            newState.company = null;
+            newState.creds = null;
             localStorage.removeItem("token");
             break;
     }
